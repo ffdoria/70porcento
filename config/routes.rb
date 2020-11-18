c9564fe0e201e1b9284a47e4597cf733c3a93c39
Andre Sato <andre.kubagawa@usp.br>
2020-11-18 12:00:15 -0300

    Finish configuration for user with first and last name

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
+  resources :premios
+  devise_for :users, :controllers => { registrations: 'registrations' }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "dashboard#index"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+500.times do |i|
+    first_name = Faker::Name.first_name
+    last_name = Faker::Name.last_name
+    User.create!(
+        first_name: first_name,
+        last_name:  last_name,
+        email: first_name + "." + last_name + i.to_s + "@usp.br",
+        password: "1234567890ab")
+        print '.' if i % 100 == 0
+  end
+  puts
