415a2abc2405778e9bc2e86eb70622e20809ba20
Andre Sato <andre.kubagawa@usp.br>
2020-11-18 13:12:36 -0300

    Create React Hello World for users search page

---
+++ app/javascript/packs/users.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function App() {
+  return <h1>Hello World</h1>;
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  ReactDOM.render(
+    <App />,
+    document.body.appendChild(document.createElement("div"))
+  );
+});
