5be24839b3fd93bef5971514a2f9d4039e748aa5
Andre Sato <andre.kubagawa@usp.br>
2020-11-18 14:02:50 -0300

    Change complement form to use radio button helper

--- app/helpers/complementos_helper.rb
+++ app/helpers/complementos_helper.rb
@@ -1,2 +1,14 @@
 module ComplementosHelper
+    def buttons(model_name, target_property, button_source)
+        html=''
+        list = button_source.sort
+        html << '<div  class="custom-control  custom-radio"><legend>Pais</legend>'
+        list.each do |x|
+            html << radio_button(model_name, target_property, x[1])
+            html << "  " + (x[0])
+            html << '<br>'
+        end
+        html << '</div>'
+        return html.html_safe
+    end
 end
