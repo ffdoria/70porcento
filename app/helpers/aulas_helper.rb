b68914d659969317138b1a21d5d53ec9675cff94
Felipe Benatti <fbcll4@gmail.com>
2020-12-13 21:42:19 -0300

    sorted

--- app/helpers/aulas_helper.rb
+++ app/helpers/aulas_helper.rb
@@ -1,2 +1,17 @@
 module AulasHelper
+
+    def dia_Hj()
+        require "date"
+        hj= Date.today
+        return hj
+    end
+
+    def orgAulas(aulas)
+        aulaOrg=[]
+        aulas.each do |aula|
+            aulaOrg.push(aula)
+        end
+        aulaOrg=aulaOrg.sort_by(data_aul)
+        return aulaOrg
+    end
 end
